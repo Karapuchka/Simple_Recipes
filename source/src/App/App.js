294d25d894e1ba39d28cbc9471f77ec570a5dab0
Karapuchka <karapu4ka2@gmail.com>
2023-04-03 17:59:19 +0300

    Изучение хука useEffect

--- source/src/App/App.js
+++ source/src/App/App.js
@@ -1,24 +1,11 @@
 import './App.scss';
-
-import Header from '../Components/Header/Header.js';
-import ModalWindow from '../Components/ModalWindow/ModalWindow';
-import { useState } from 'react';
+import List from '../components/envelope/Envelope.js';
 
 function App() {
 
-  const [openModal, setOpenModal] = useState(false);
-
-  const setStateModal = ()=>{
-
-    setOpenModal(openModal => !openModal);
-
-  }
-
   return (
-    <div className="wrapper">
-      <Header      setStateModal={setStateModal}/>
-      <ModalWindow stateModal={openModal} setStateModal={setStateModal}/>
-      
+    <div className={`wrapper`}>
+      <List />
     </div>
   )
 }
--- source/src/App/detect.js
+++
@@ -1,7 +0,0 @@
-//Переменная для работы с устройством пользователя
-
-import MobileDetect from 'mobile-detect';
-
-let detect = new MobileDetect(window.navigator.userAgent);
-
-export default detect;
